897cfabdb25da476393fae7131460779ff168c59
p-prince-n <pp2358296@gmail.com>
2025-04-21 09:16:05 +0530

    deploying

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from"express";
+import authRoute from "./Src/routes/auth.route.js";
+import messageRoute from "./Src/routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./Src/lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./Src/lib/socket.js";
+
+
+dotenv.config();
+
+const PORT=process.env.PORT;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+app.use(cookieParser())
+app.use(express.json({limit: '2mb'}));
+app.use('/api/auth', authRoute);
+app.use('/api/messages', messageRoute);
+
+
+
+server.listen(PORT, ()=>{
+    console.log(`server started at http://localhost:${PORT}`);
+    connectDB();
+})
